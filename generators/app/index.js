d094559dede03827fb9b68d790e72742c8548d2e
itowlson <ivan.towlson@microsoft.com>
2020-09-11 11:55:23 +1200

    Ask more questions and template more stuff

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,12 +14,26 @@ const TEMPLATE_FILES = [
 
 module.exports = class extends Generator {
   prompting() {
+    const username = this.user.git.name() || process.env.USER || process.env.USERNAME;
+
     const prompts = [
       {
         type: "input",
         name: "moduleName",
         message: "What is the name of the WASM module?",
         default: this.appname
+      },
+      {
+        type: "input",
+        name: "authorName",
+        message: "What is the name of the author?",
+        default: username
+      },
+      {
+        type: "input",
+        name: "registryName",
+        message: "What registry do you plan to publish the module to (currently ACR only)?",
+        default: username + "acrwasm"
       }
     ];
 
--- generators/app/templates/README.md
+++ generators/app/templates/README.md
@@ -1,5 +1,5 @@
 # <%= moduleName %>
 
-A test project for templating a WASM hello-world with a publish action
+A WASM hello-world with a publish action
 
 To build: `cargo build --target wasm32-wasi --release`
